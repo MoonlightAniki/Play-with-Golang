b51d4235fea0b50894d7daaa6e18b536eec60fa4
guoliang <guoliang0511@qq.com>
2018-11-22 15:56:51 +0800

    循环

---
+++ HelloGo/loop.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"os"
+	"bufio"
+)
+
+func sum(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum
+}
+
+// 将十进制数组转成二进制字符串
+func convertToBinaryString(n int) string {
+	if n == 0 {
+		return "0"
+	}
+	res := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		res = strconv.Itoa(lsb) + res
+	}
+	return res
+}
+
+// 逐行打印文件中的文本
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	// for 语句无循环初始条件和循环递增条件的话可以省略分号
+	// 等价于java语言中的while语句
+	// go语言中没有while关键字
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+// 死循环
+func forever() {
+	// true 可以省略
+	for {
+		fmt.Println("abc")
+	}
+}
+
+func main() {
+	fmt.Println(sum(100))
+	fmt.Println(
+		convertToBinaryString(10),
+		convertToBinaryString(13),
+		convertToBinaryString(32),
+		convertToBinaryString(23333),
+		convertToBinaryString(0),
+	)
+
+	printFile("abc.txt")
+
+	forever()
+}
